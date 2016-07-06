4c23a0900e6443c7456a7fceaec0ce50ceeee541
rostyslav_y <r.yeromchenko@onlini.co>
2016-07-06 14:02:35 +0300

    Changed dim amount and fixed spelling mistake SimpleFullscreenProgressDialog.

--- iua-fullscreen-progress-dialog/src/main/java/ua/in/iua/iua_fullscreen_progress_dialog/SimpleFullscreenProgressDialog.java
+++ iua-fullscreen-progress-dialog/src/main/java/ua/in/iua/iua_fullscreen_progress_dialog/SimpleFullscreenProgressDialog.java
@@ -18,27 +18,29 @@ public class SimpleFullscreenProgressDialog extends Dialog {
         super(context);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        View view = inflater.inflate(R.layout.fullscreen_progress_dialog, null, false);
+        View view = inflater.inflate(ua.in.iua.iua_fullscreen_progress_dialog.R.layout.fullscreen_progress_dialog, null, false);
 
         int tintColor = ActivityCompat.getColor(context, tintColorResource);
 
-        ProgressBar progressBar = (ProgressBar) view.findViewById(R.id.progressBar);
+        ProgressBar progressBar = (ProgressBar) view.findViewById(ua.in.iua.iua_fullscreen_progress_dialog.R.id.progressBar);
         progressBar.getIndeterminateDrawable().setColorFilter(tintColor, PorterDuff.Mode.MULTIPLY);
 
-        TextView tvMessage = (TextView) view.findViewById(R.id.tvMessage);
+        TextView tvMessage = (TextView) view.findViewById(ua.in.iua.iua_fullscreen_progress_dialog.R.id.tvMessage);
         tvMessage.setTextColor(tintColor);
         tvMessage.setText(message);
 
         setContentView(view);
         getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
+        getWindow().setDimAmount(0.7f);
+        setCancelable(false);
     }
 
     public SimpleFullscreenProgressDialog(Context context) {
-        this(context, context.getString(R.string.dialog_progress_default_text), R.color.colorProgressDialog);
+        this(context, context.getString(ua.in.iua.iua_fullscreen_progress_dialog.R.string.dialog_progress_default_text), ua.in.iua.iua_fullscreen_progress_dialog.R.color.colorProgressDialog);
     }
 
-    public static Dialog showDialog(Context context, String message, @ColorRes int tintColorResourse) {
-        Dialog dialog = new Dialog(context);
+    public static Dialog showDialog(Context context, String message, @ColorRes int tintColorResource) {
+        Dialog dialog = new SimpleFullscreenProgressDialog(context, message, tintColorResource);
         dialog.show();
         return dialog;
     }
